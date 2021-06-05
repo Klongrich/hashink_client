db3faf652510cbec1ee560d9ca8a10cbca26ee46
Kyle Longrich <klongrich@kyles-mbp.home>
2021-06-04 21:08:16 -0400

    first push

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from  "react";
+
+import Header from "./Components/header"
+
+function App() {
+  return (
+    <>
+      <Header />
+    </>
+  );
+}
+
+export default App;
---
+++ src/Components/header.js
@@ -0,0 +1,70 @@
+import React from "react";
+import styled from "styled-components";
+
+import  LogoImage from "../Images/ink2.svg";
+import LogoText from "../Images/ink-long.svg";
+import { ShoppingCart } from "@styled-icons/evaicons-solid/ShoppingCart";
+
+const Container = styled.div`
+    background-color: white;
+
+    ul {
+        list-style-type: none;
+        display: inline-block;
+        margin-top: 29px;
+    }
+
+    li {
+        float: left;
+        margin-left: 20px;
+        padding-right: 20px;
+        color: grey;
+        border-right: 1px solid grey;
+    }
+`
+
+const CartButton = styled.div`
+    background-color: grey;
+    color: white;
+    width: 75px;
+    height: 25px;
+    padding: 5px;
+    margin-left: 20px;
+    margin-top: 19px;
+`
+
+const FloatBox = styled.div`
+    float: left;
+`
+
+export default function Header () {
+
+    return (
+        <>
+        <Container>
+
+            <FloatBox>
+                <img src={LogoImage} width="75" height="75" />
+                <img src={LogoText} width="110" height="75" />
+            </FloatBox>
+            
+            <FloatBox>
+            <ul>
+                <li>Home</li>
+                <li>Shop</li>
+                <li>Gallery</li>
+                <li>Help</li>
+            </ul>
+            </FloatBox>
+
+            <FloatBox>
+            <CartButton>
+                <ShoppingCart size="25" />
+                Cart
+            </CartButton>
+            </FloatBox>
+        </Container>
+        </>
+    )
+
+}
