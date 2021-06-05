bb51a57c1c9ad97dd11b4b62e6e42df5c11c3709
Kyle Longrich <klongrich@kyles-mbp.home>
2021-06-04 22:42:35 -0400

    json data needed to render client

---
+++ src/Data/homePageData.js
@@ -0,0 +1,106 @@
+export const rankingData = [
+    {
+        id: 0,
+        ranking: "1",
+        name: "Micheal Jackson",
+        price: "$800",
+        image: null,
+    },
+    {
+        id: 1,
+        ranking: "2",
+        name: "Vitalik Buterin",
+        price: "0.2 ETH",
+        image: null,
+    },
+    {
+        id: 2,
+        ranking: "3",
+        name: "Beeple",
+        price: "$123",
+        image: null,
+    },
+    {
+        id: 3,
+        ranking: "4",
+        name: "John Lennon",
+        price: "$55",
+        image: null,
+    },
+    {
+        id: 4,
+        ranking: "5",
+        name: "Mick Jagger",
+        price: "$800",
+        image: null,
+    },
+    {
+        id: 5,
+        ranking: "6",
+        name: "Al Pacino",
+        price: "0.2 ETH",
+        image: null,
+    }
+]
+
+
+export const spotLightData = [
+    {
+        id: 0,
+        name: "Testing",
+        title: "CEO",
+        image: null,
+    },
+    {
+        id: 1,
+        name: "Testing Two",
+        title: "CTO",
+        image: null,
+    },
+    {
+        id: 2,
+        name: "Testing Three",
+        title: "CFO",
+        image: null
+    }
+]
+
+
+export const seeMoreData = [
+    {
+        id: 0,
+        name: "Product Name: ",
+        price: "$300",
+        rating: "3 stars",
+    },
+    {
+        id: 1,
+        name: "Product Name: ",
+        price: "$300",
+        rating: "3 stars",
+    },   
+    {
+        id: 2,
+        name: "Product Name: ",
+        price: "$300",
+        rating: "3 stars",
+    },    
+    {
+        id: 3,
+        name: "Product Name: ",
+        price: "$300",
+        rating: "3 stars",
+    },    
+    {
+        id: 4,
+        name: "Product Name: ",
+        price: "$300",
+        rating: "3 stars",
+    },    
+    {
+        id: 5,
+        name: "Product Name: ",
+        price: "$300",
+        rating: "3 stars",
+    }
+]
