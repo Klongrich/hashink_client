5fcde363b484da3a6edd93deb340b91613da2ce7
Kyle Longrich <klongrich@kyles-mbp.home>
2021-06-04 22:43:04 -0400

    updated

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from  "react";
 
 import Header from "./Components/header"
 import HomePage from "./Components/homepage"
+import Footer from "./Components/footer"
 
 function App() {
   return (
     <>
       <Header />
-      <HomePage />
+        <HomePage />
+      <Footer />
     </>
   );
 }
